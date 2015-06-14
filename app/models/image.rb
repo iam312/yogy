652b2d151f7f950fdd1bc379a4c734461de1b175
Samil Goh <iam312@gmail.com>
2015-06-14 22:33:48 +0900

    iam312/yogy#9 이미지 목록에도 캐싱 추가

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,7 @@
 class Image < ActiveRecord::Base
-  #validates :asset, presence: true
   validates :s3, presence: true
   validates :user_id, presence: true
 
-  #mount_uploader :asset, ImageUploader
   mount_uploader :s3, S3Uploader
 
   belongs_to :user
